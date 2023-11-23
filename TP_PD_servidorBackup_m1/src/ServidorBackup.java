9de43c4fbc8cbe1b7c7cdc39209ebbddf9f7a254
Chelsea Duarte <a2021100010@isec.pt>
2023-11-23 03:26:18 +0000

    falta organizar o código, rever algumas coisas, etc

--- TP_PD_servidorBackup_m1/src/ServidorBackup.java
+++ TP_PD_servidorBackup_m1/src/ServidorBackup.java
@@ -1,3 +1,4 @@
+import pt.isec.pd.trabalhoPratico.model.dataAccess.DbManager;
 import pt.isec.pd.trabalhoPratico.model.recordDados.DadosRmi;
 import pt.isec.pd.trabalhoPratico.model.RemoteInterface;
 
@@ -7,6 +8,8 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ServidorBackup {
     private String registration;
@@ -16,9 +19,38 @@ public class ServidorBackup {
     private static final int portobackup=4444;
     private final String Heartbeatip="230.44.44.44";
     private InetAddress group;
+    private Timer timeoutTimer = new Timer();
+
+    private DbManager dbManager = new DbManager();
 
 
     public static void main(String[] args) {
+
+/*
+        // Verifica se o número de argumentos é válido
+        if (args.length != 1) {
+            System.err.println("Sintaxe: ServidorBackup <caminho_da_diretoria>");
+            System.exit(1);
+        }
+
+        String diretoria = args[0];
+
+        // Verifica se o caminho existe
+        File caminho = new File(diretoria);
+
+        if (!caminho.exists() || !caminho.isDirectory()) {
+            System.err.println("[Erro] O caminho especificado nao corresponde a uma diretoria valida");
+            System.exit(1);
+        }
+
+        // Verifica se a diretoria está vazia
+        if (caminho.list().length > 0) {
+            System.err.println("A diretorio não esta vazia. A encerrar o servidor backup...");
+            System.exit(1);
+        }
+
+*/
+
         ServidorBackup servidorBackup = new ServidorBackup();
         Heartbeat heartbeatThread = servidorBackup.new Heartbeat();
 
@@ -55,34 +87,61 @@ public class ServidorBackup {
         }
     }
 
+
+
+    class TimeoutTask extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("[Timeout] Nenhum heartbeat recebido. A encerrar o servidor backup...");
+            System.exit(0); //por enquanto ainda não implementei nada melhor para sair
+        }
+    }
     class Heartbeat extends Thread{// im not sure pq é que criei este thread  mas agr ta criadad
+
         @Override
         public void run() {
            try(MulticastSocket multicastSocket=new MulticastSocket(portobackup)){
                group=InetAddress.getByName(Heartbeatip);
-                multicastSocket.joinGroup(group);
-
-
+               multicastSocket.joinGroup(group);
 
                DatagramPacket packet=new DatagramPacket(new byte[2024],2024);// aqui tenho de por um valor diferente i guess
-               multicastSocket.receive(packet);
-               ByteArrayInputStream bye=new ByteArrayInputStream(packet.getData(),0, packet.getLength());
-               ObjectInputStream oin=new ObjectInputStream(bye);
-               DadosRmi dados= (DadosRmi) oin.readObject();
 
-               if(!conected){
-                    registration="rmi://"+dados.Registo()+"/"+dados.nome_servico();
-                    rmi= (RemoteInterface) Naming.lookup(registration);
-                    conected=true;
-                    System.out.println("Servidor de backup conectado ao servidor principal");
+               TimeoutTask timeoutTask = new TimeoutTask();
 
-                   receiveDb(); //recebe a copia da base de dados do servidor principal
+               while (true) {
+                   multicastSocket.receive(packet);
+                   ByteArrayInputStream bye = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
+                   ObjectInputStream oin = new ObjectInputStream(bye);
+                   DadosRmi dados = (DadosRmi) oin.readObject();
 
-                   String backupIpAddress = InetAddress.getLocalHost().getHostAddress();
-                   String backupServiceURL = "rmi://" + backupIpAddress + "/" + dados.nome_servico();
-                   // Registra o servidor de backup para callbacks (rever isso ???)
-                   rmi.registaBackupServers(backupServiceURL);
+                   if (!conected) {
+                       registration = "rmi://" + dados.Registo() + "/" + dados.nome_servico();
+                       rmi = (RemoteInterface) Naming.lookup(registration);
+                       conected = true;
+                       System.out.println("Servidor de backup conectado ao servidor principal");
 
+                       receiveDb(); //recebe a copia da base de dados do servidor principal
+
+
+                       // Compara a versão da base de dados recebida com a versão local
+                       if (dados.versao() != dbManager.getVersaoDb()) {
+                           System.out.println("Versão da base de dados diferente. A encerrar o servidor backup...");
+                           System.exit(0);
+                       }
+
+
+                       // Regista o servidor de backup para callbacks (rever isso ???)
+                       String backupIpAddress = InetAddress.getLocalHost().getHostAddress();
+                       String backupServiceURL = "rmi://" + backupIpAddress + "/" + dados.nome_servico();
+
+                       rmi.registaBackupServers(backupServiceURL);
+
+                       // Reinicia o timer após receber um heartbeat
+                       timeoutTimer.cancel();
+                       timeoutTimer = new Timer();
+                       timeoutTimer.schedule(timeoutTask, 30000);
+
+                   }
                }
 
            } catch (IOException e) {
---
+++ TP_PD_servidorBackup_m1/src/pt/isec/pd/trabalhoPratico/model/dataAccess/DbManager.java
@@ -0,0 +1,41 @@
+package pt.isec.pd.trabalhoPratico.model.dataAccess;
+
+
+import java.io.File;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+public class DbManager {
+    private static final String dbAdress = "copiaDb.db";
+    private static final String dbUrl= "jdbc:sqlite:"+dbAdress;
+    public static String getDbAdress() {
+        return dbAdress;
+    }
+
+    public int getVersaoDb(){
+        try(Connection connection=DriverManager.getConnection(dbUrl)) {
+            String GetQuery="Select versao_id FROM VERSAO";
+            PreparedStatement statement=connection.prepareStatement(GetQuery);
+            ResultSet rs= statement.executeQuery();
+            if(rs.isBeforeFirst()){
+                rs.next();
+                return rs.getInt("versao_id");
+
+            }else{
+                return 0;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
